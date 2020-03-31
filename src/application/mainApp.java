4b287f18de63ea172c94609e22c286448c125ef2
nsmlkz <nassimlaar@gmail.com>
2020-03-31 14:37:03 -0400

    test nass

--- src/application/mainApp.java
+++ src/application/mainApp.java
@@ -6,6 +6,7 @@ public class mainApp {
         System.out.println("test NL");
         System.out.println("Salut");
         System.out.println("test ryad");
+        System.out.println("test nassim 2");
 
     }
 }
