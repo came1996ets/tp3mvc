55420f370bdbb0f94ecd18c2ccc08c38b1ddbe6f
MrRyado <ryad.meftahi@gmail.com>
2020-03-31 14:28:56 -0400

    test commit ryad

--- src/application/mainApp.java
+++ src/application/mainApp.java
@@ -5,6 +5,7 @@ public class mainApp {
     public static void main(String[] args){
         System.out.println("test NL");
         System.out.println("Salut");
+        System.out.println("test ryad");
 
     }
 }
